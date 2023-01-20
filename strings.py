f3e6d43839fba275b4a47bce0d8497c0a0350426
vagrant <vagrant@localhost.localdomain>
2023-01-20 08:58:50 +0000

    changed script to run with pytest insted of printing the result

--- strings.py
+++ strings.py
@@ -1,41 +1,45 @@
+
+import pytest
+
 mystring= 'monty pythons flying circus'
 
 def no_duplicates(a_string):
     p= ""
     for char in a_string:
         if char not in p:
-            p= p + char
+            p = p + char
     p= ''.join(sorted(p))
-    print(p)
-#    return p
+#    print(f" {p}")
+    return p
 
 
 
 def reversed_words(a_string):
     a_string= a_string.split(" ")
     a_string.reverse()
-    print(a_string)
-#    return a_string
+#    print(f" {a_string}")
+    return a_string
 
 def four_char_strings(a_string):
     n= 4
     chunks= []
     for i in range(0, len(a_string), n):
         chunks.append(a_string[i:i + n])
-    print(chunks)
-#    return chunks
+#    print(f" {chunks}")
+    return chunks
 
 
-
-s='monty pythons flying circus'
-no_duplicates(s)
-
-
-s= 'monty pythons flying circus'
-reversed_words(s)
+def test_no_duplicates():
+    s ='monty pythons flying circus'
+    assert no_duplicates(s) == ' cfghilmnoprstuy'
 
 
-s= 'monty pythons flying circus'
-four_char_strings(s)
-[vagrant@centos7-slave ~]$
+def test_reversed_words():
+    s= 'monty pythons flying circus'
+    assert reversed_words(s) == ['circus', 'flying', 'pythons', 'monty']
+
+
+def test_four_char_strings():
+    s= 'monty pythons flying circus'
+    assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 
